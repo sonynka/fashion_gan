b6f89af43631c9f7008a7b937f987517c1bdfb1e
Sona Pecenakova <sona.pecenakova@gmail.com>
2018-09-22 16:44:06 +0200

    refactor and document

---
+++ utils/feature_utils.py
@@ -0,0 +1,147 @@
+import numpy as np
+import os
+import io
+import requests
+from PIL import Image
+import torch
+import torchvision
+import torch.nn as nn
+from torchvision import transforms
+
+
+class ResnetFeatureGenerator():
+    """
+    Loads a ResNet152 model to generate feature vectors from the last hidden
+    layer of the network. If no model path is provided, the pretrained ImageNet
+    model from PyTorch is loaded. Otherwise, the model's weights are loaded
+    from the model path.
+    """
+
+    _DATA_TRANSFORMS = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    def __init__(self, retrained_model_path=None):
+        """
+        :param retrained_model_path: path to a retrained resnet152 model
+        (if not provided, the original ImageNet pretrained model from PyTorch
+        is loaded)
+        """
+
+        self.model_path = retrained_model_path
+        self.model = self.load_resnet152()
+
+    def get_feature(self, img: Image):
+        feature = self.model(self._DATA_TRANSFORMS(img).unsqueeze(0))
+        feature = feature.squeeze().data.numpy()
+
+        return feature
+
+    def load_resnet152(self):
+        """Loads original ResNet152 model with pretrained weights on ImageNet
+        dataset. If a model_path is provided, it loads the weights from the
+        model_path. Returns the model excluding the last layer.
+        """
+
+        model = torchvision.models.resnet152(pretrained=True)
+
+        if self.model_path:
+            num_ftrs = model.fc.in_features
+            model.fc = nn.Linear(num_ftrs, 7)
+            model.load_state_dict(
+                torch.load(self.model_path, map_location='cpu'))
+
+        # only load layers until the last hidden layer to extract features
+        modules = list(model.children())[:-1]
+        model = nn.Sequential(*modules)
+
+        return model
+
+
+class AkiwiFeatureGenerator():
+    """
+    Queries the Akiwi API to get feature vectors for an image.
+    """
+
+    SIZE_URLS = {
+        64:     ['http://akiwi.eu/mxnet/feature/'],
+        50:     ['http://akiwi.eu/feature/fv50/'],
+        114:    ['http://akiwi.eu/mxnet/feature/',
+                 'http://akiwi.eu/feature/fv50/']
+    }
+
+    def __init__(self, feature_size):
+        """
+        Based on the feature size, queries the corresponding API endpoint.
+        Feature size 114 are sizes 64 and 50 appended.
+        :param feature_size: the size of the feature vector to download
+        """
+        self.urls = self.SIZE_URLS[feature_size]
+
+    def get_feature(self, img: Image):
+
+        features = []
+        for url in self.urls:
+            f = self._get_url_feature(url, img)
+            features.append(f)
+
+        feature = np.concatenate(features)
+        return feature
+
+    @staticmethod
+    def _get_url_feature(url, img: Image):
+        """
+        Query the Akiwi API to get an image feature vector
+        :param url: URL of the API to query
+        :param img: image for which to get feature vector
+        :return: feature numpy array
+        """
+
+        img_bytes = io.BytesIO()
+        img.save(img_bytes, format='JPEG')
+        files = {'file': ('img.jpg', img_bytes.getvalue(), 'image/jpeg')}
+        response = requests.post(url, files=files, timeout=10)
+
+        if response.status_code == 200:
+            response_feature = response.content
+            feature = np.frombuffer(response_feature, dtype=np.uint8)
+            return feature
+
+        print("Couldn't get feature. Response: ", response)
+
+
+def download_feature_vectors(files, save_dir, feature_generator=None):
+    """
+    Downloads feature vectors for a list of files.
+    :param files: list of files
+    :param save_dir: directory to save feature vectors
+    :param feature_generator: feature generator to create features
+    """
+
+    if not feature_generator:
+        feature_generator = AkiwiFeatureGenerator(114)
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    for idx, file in enumerate(files):
+        if idx % 100 == 0:
+            print('Downloaded {} / {}'.format(idx, len(files)))
+
+        # name feature with the same basename as file
+        save_path = os.path.join(save_dir, os.path.basename(file).split('.jpg')[0] + '.npy')
+        if os.path.exists(save_path):
+            continue
+
+        feature = feature_generator.get_feature(Image.open(file))
+        np.save(save_path, feature)
+
+
+def main():
+    print('done')
+
+
+if __name__ == '__main__':
+    main()
--- utils/search_utils.py
+++ utils/search_utils.py
@@ -1,25 +1,130 @@
 import numpy as np
 import os
 import glob
-import io
-import requests
 from PIL import Image
-import torch
-import torchvision
-import torch.nn as nn
-from torchvision import transforms
 from sklearn.metrics import pairwise_distances
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from utils import image_utils
+from utils.feature_utils import ResnetFeatureGenerator, AkiwiFeatureGenerator
+
+
+class Search():
+    """
+    Loads all features from a folder and searches best images by comparing
+    the distances between the features.
+    """
+
+    def __init__(self, images_root, features_root, feature_generator=None):
+        """
+        :param features_root: path to folder containing .npy features
+        :param images_root: path to folder containg images with same names as
+                            in features_root
+        :param feature_generator: generator to create features for new images
+        """
+
+        if not feature_generator:
+            feature_generator = ResnetFeatureGenerator()
+
+        self.features_root = features_root
+        self.feature_generator = feature_generator
+        self.images_root = images_root
+
+        self.feature_names, self.features = self._load_features()
+        self.scaler = self._scale_features()
+
+    def _load_features(self):
+        """
+        Load features from the feature root into a numpy array
+        """
+
+        print('Loading features from:', self.features_root)
+        feat_files = glob.glob(os.path.join(self.features_root, '*.npy'))
+
+        if len(feat_files) == 0:
+            raise ValueError(
+                'Features root {} is empty.'.format(self.features_root))
+
+        feat_files = sorted(feat_files)
+        feat_names = [os.path.basename(f).rsplit('.', 1)[0]
+                      for f in feat_files]
+        features = np.array([np.load(f) for f in feat_files])
+
+        return feat_names, features
+
+    def _scale_features(self):
+        """
+        Scale all loaded features to mean=0 and std=1
+        """
+        scaler = StandardScaler()
+        self.features = scaler.fit_transform(self.features)
+
+        return scaler
+
+    def _get_img_feature(self, img):
+        img = image_utils.process_image(img)
+
+        img_feature = self.feature_generator.get_feature(img)
+        img_feature = img_feature.reshape(1, -1)
+        img_feature = self.scaler.transform(img_feature)
+
+        return img_feature
+
+    def _search_similar_images(self, img, num_imgs, metric):
+        """
+        Calculate pairwise distances from the given image to the loaded
+        features and return the image paths with the smallest distances
+        """
+
+        img_feature = self._get_img_feature(img)
+
+        dist = pairwise_distances(img_feature, self.features, metric=metric)
+        best_img_idxs = np.argsort(dist)[0].tolist()[:num_imgs]
+        best_img_dist = dist[0][best_img_idxs]
+
+        best_img_paths = [
+            os.path.join(self.images_root, self.feature_names[i] + '.jpg')
+            for i in best_img_idxs]
+
+        return best_img_paths, best_img_dist
+
+    def get_similar_images_with_distances(self, img: Image, num_imgs=8, metric='l1'):
+        """
+        Retrieve similar images and their distances to the given image compared
+        with the given metric
+        :param img: image for which to find similar images
+        :param num_imgs: number of similar images to retrieve
+        :param metric: distance metric to use when comparing features ['l1','l2']
+        :return: best image paths and their distances to the original image
+        """
+        return self._search_similar_images(img, num_imgs, metric)
+
+    def get_similar_images(self, img: Image, num_imgs=8, metric='l1'):
+        """
+        Retrieve similar images to the given image compared with the given metric
+        :param img: image for which to find similar images
+        :param num_imgs: number of similar images to retrieve
+        :param metric: distance metric to use when comparing features ['l1','l2']
+        :return: best image paths
+        """
+
+        best_img_paths, _ = self._search_similar_images(img, num_imgs, metric)
+        return best_img_paths
 
 
 class CombinedSearch():
+    """
+    Combines a list of Search classes to enable search with combined features
+    """
 
     def __init__(self, search_list, factors=None):
         """
+        :param search_list: list of Search objects
+        :param factors: list of factors to multiply the respective Search features
         """
+
+        # take all features as equal
         if not factors:
-            factors = [1, 1]
+            factors = [1] * len(search_list)
 
         self.factors = factors
         self.search_list = search_list
@@ -27,10 +132,9 @@ class CombinedSearch():
 
         for search, factor in zip(search_list, factors):
             self.features.append(factor * search.features)
-
         self.features = np.concatenate(self.features, axis=1)
 
-    def get_similar_images(self, img, num_imgs=8, metric='l1', get_distances=False):
+    def _get_img_feature(self, img):
         img = image_utils.process_image(img)
 
         img_feature = []
@@ -43,191 +147,54 @@ class CombinedSearch():
         img_feature = np.concatenate(img_feature)
         img_feature = img_feature.reshape(1, -1)
 
+        return img_feature
+
+    def _search_similar_images(self, img, num_imgs, metric):
+        """
+        Calculate pairwise distances from the given image to the loaded
+        features and return the image paths with the smallest distances
+        """
+
+        img_feature = self._get_img_feature(img)
+
         dist = pairwise_distances(img_feature, self.features, metric=metric)
         best_img_idxs = np.argsort(dist)[0].tolist()[:num_imgs]
         best_img_dist = dist[0][best_img_idxs]
 
-        best_img_paths = [os.path.join(self.search_list[0].images_root,
-                                  self.search_list[0].feature_names[i] + '.jpg')
+        best_img_paths = [
+            os.path.join(self.search_list[0].images_root,
+                         self.search_list[0].feature_names[i] + '.jpg')
             for i in best_img_idxs]
 
-        if get_distances:
-            return best_img_paths, best_img_dist
-        else:
-            return best_img_paths
+        return best_img_paths, best_img_dist
 
-class Search():
-
-    def __init__(self, images_root, features_root,
-                 feature_generator=None):
+    def get_similar_images_with_distances(self, img: Image, num_imgs=8, metric='l1'):
         """
-        Loads all the given features and searches best images by comparing
-        the L2 distances.
-        :param features_root:   path to folder containing .npy features
-        :param images_root:     path to folder containg images with same names
-                                as in features_root
+        Retrieve similar images and their distances to the given image compared
+        with the given metric
+        :param img: image for which to find similar images
+        :param num_imgs: number of similar images to retrieve
+        :param metric: distance metric to use when comparing features ['l1','l2']
+        :return: best image paths and their distances to the original image
+        """
+        return self._search_similar_images(img, num_imgs, metric)
+
+    def get_similar_images(self, img: Image, num_imgs=8, metric='l1'):
+        """
+        Retrieve similar images to the given image compared with the given metric
+        :param img: image for which to find similar images
+        :param num_imgs: number of similar images to retrieve
+        :param metric: distance metric to use when comparing features ['l1','l2']
+        :return: best image paths
         """
 
-        if not feature_generator:
-            feature_generator = ResnetFeatureGenerator()
+        best_img_paths, _ = self._search_similar_images(img, num_imgs, metric)
+        return best_img_paths
 
-        self.features_root = features_root
-        self.feature_generator = feature_generator
-        self.images_root = images_root
-
-        self.feature_names, self.features = self.load_features()
-        self.scaler = self.scale_features()
-
-    def load_features(self):
-        print('Loading features from:', self.features_root)
-        feat_files = glob.glob(os.path.join(self.features_root, '*.npy'))
-
-        if len(feat_files) == 0:
-            raise ValueError('Features root is empty.')
-
-        feat_files = sorted(feat_files)
-
-        feat_names = [os.path.basename(f).rsplit('.', 1)[0]
-                      for f in feat_files]
-        features = np.array([np.load(f) for f in feat_files])
-
-        return feat_names, features
-
-    def scale_features(self):
-        scaler = StandardScaler()
-        self.features = scaler.fit_transform(self.features)
-
-        return scaler
-
-    def get_similar_images(self, img: Image, num_imgs=8, metric='l1', get_distances=False):
-        img = image_utils.process_image(img)
-
-        img_feature = self.feature_generator.get_feature(img)
-        img_feature = img_feature.reshape(1, -1)
-        img_feature = self.scaler.transform(img_feature)
-
-        dist = pairwise_distances(img_feature, self.features, metric=metric)
-        best_img_idxs = np.argsort(dist)[0].tolist()[:num_imgs]
-        best_img_dist = dist[0][best_img_idxs]
-
-        best_img_paths = [os.path.join(self.images_root, self.feature_names[i] + '.jpg')
-                     for i in best_img_idxs]
-
-        if get_distances:
-            return best_img_paths, best_img_dist
-        else:
-            return best_img_paths
-
-class ResnetFeatureGenerator():
-
-    data_transforms = transforms.Compose([
-        transforms.Resize(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-
-    def __init__(self, retrained_model_path=None):
-
-        self.model_path = retrained_model_path
-        self.model = self.load_resnet152()
-
-    def get_feature(self, img: Image):
-        feature = self.model(self.data_transforms(img).unsqueeze(0))
-        feature = feature.squeeze().data.numpy()
-
-        return feature
-
-    def load_resnet152(self):
-        model = torchvision.models.resnet152(pretrained=True)
-
-        if self.model_path:
-            num_ftrs = model.fc.in_features
-            model.fc = nn.Linear(num_ftrs, 7)
-            model.load_state_dict(torch.load(self.model_path,
-                                             map_location='cpu'))
-
-        modules = list(model.children())[:-1]
-        model = nn.Sequential(*modules)
-
-        return model
-
-
-class AkiwiFeatureGenerator():
-
-    SIZE_URLS = {
-        64:     ['http://akiwi.eu/mxnet/feature/'],
-        50:     ['http://akiwi.eu/feature/fv50/'],
-        114:    ['http://akiwi.eu/mxnet/feature/',
-                 'http://akiwi.eu/feature/fv50/']
-    }
-
-    def __init__(self, feature_size):
-        self.urls = self.SIZE_URLS[feature_size]
-
-    def get_feature(self, img: Image):
-
-        features = []
-        for url in self.urls:
-            f = self.__get_url_feature(url, img)
-            features.append(f)
-
-        feature = np.concatenate(features)
-
-        return(feature)
-
-    def __get_url_feature(self, url, img):
-
-        img_bytes = io.BytesIO()
-        img.save(img_bytes, format='JPEG')
-        files = {'file': ('img.jpg', img_bytes.getvalue(), 'image/jpeg')}
-        response = requests.post(url, files=files, timeout=10)
-
-        if response.status_code == 200:
-            response_feature = response.content
-            feature = np.frombuffer(response_feature, dtype=np.uint8)
-            return feature
-        else:
-            print("Couldn't get feature. Response: ", response)
-
-
-def download_feature_vectors(files, save_dir, feature_gen=None):
-    if not feature_gen:
-        feature_gen = AkiwiFeatureGenerator(114)
-
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    for idx, file in enumerate(files):
-        if idx % 100 == 0:
-            print('Downloaded {} / {}'.format(idx, len(files)))
-
-        save_path = os.path.join(save_dir, os.path.basename(file).split('.jpg')[0] + '.npy')
-        if os.path.exists(save_path):
-            continue
-
-        feature = feature_gen.get_feature(Image.open(file))
-        np.save(save_path, feature)
 
 def main():
-    product_imgs = '/Users/sonynka/HTW/MasterArbeit/data/fashion/dresses/'
-    product_feats_root = '/Users/sonynka/HTW/MasterArbeit/Projects/fashion_designer/data/features/fashion/dresses/'
-
-    folder_gens = {'akiwi_50': AkiwiFeatureGenerator(50),
-                   'akiwi_64': AkiwiFeatureGenerator(64),
-                   'akiwi_114': AkiwiFeatureGenerator(114),
-                   'resnet': ResnetFeatureGenerator(),
-                   'resnet_retrained': ResnetFeatureGenerator(
-                       './models/resnet152_retrained.pth')
-                   }
-
-    searches = {}
-    for dir_name, gen in folder_gens.items():
-        searches[dir_name] = Search(product_imgs,
-                                    os.path.join(product_feats_root, dir_name),
-                                    gen)
-
-    searches['akiwi_50'].get_similar_images(Image.open('/Users/sonynka/HTW/MasterArbeit/data/fashion/dresses/5713733606269.jpg'))
     print('done')
+
 
 if __name__ == '__main__':
     main()
