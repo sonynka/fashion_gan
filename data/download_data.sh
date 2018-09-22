aee2445a0c9be996718c0389dfb47fdc8fd47e12
Sona Pecenakova <sona.pecenakova@gmail.com>
2018-09-22 22:48:40 +0200

    add script for downloading data

---
+++ data/download_data.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+echo "Downloading images..."
+curl -Lo images.zip https://s3.eu-central-1.amazonaws.com/fashion-gan/images.zip
+echo "Downloading features..."
+curl -Lo features.zip https://s3.eu-central-1.amazonaws.com/fashion-gan/features.zip
+echo "Downloading models..."
+curl -Lo models.zip https://s3.eu-central-1.amazonaws.com/fashion-gan/models.zip
+
+echo "Unzipping files..."
+unzip -q images.zip
+unzip -q features.zip
+unzip -q models.zip
+
+rm *.zip
